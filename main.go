2173f517b514740323a3b978c691e3c7bb05440a
Spankie Dee <odohi.david@gmail.com>
2018-10-04 09:57:34 +0100

    chore(route): set up assets route and index route

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Small Golang web app boiler plate.")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	filesDir := "public"
+	fileServer := http.StripPrefix("/assets/", http.FileServer(http.Dir(filesDir)))
+	r.Get("/assets/*", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Vary", "Accept-Encoding")
+		w.Header().Set("Cache-Control", "public, max-age=7776000")
+		fileServer.ServeHTTP(w, r)
+	})
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./public/index.html")
+	})
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./public/index.html")
+	})
+
+	log.Print("Server started at :3333")
+	http.ListenAndServe(":3333", r)
 }
